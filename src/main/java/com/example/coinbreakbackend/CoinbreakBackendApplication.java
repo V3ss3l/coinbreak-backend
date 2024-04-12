d84ad5daeace8b6b70d2224ff06a58d805dfa410
v3ss3l <grishav3ss3l@gmail.com>
2024-04-12 14:07:41 +0400

    Initial commit

---
+++ src/main/java/com/example/coinbreakbackend/CoinbreakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coinbreakbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbreakBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinbreakBackendApplication.class, args);
+    }
+
+}
